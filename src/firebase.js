da5e7cb1bad0f80ebe8f52151c86e163fc1a5df5
Michael Kumsa <85396282+mikael-kumsa@users.noreply.github.com>
2025-11-09 19:57:26 +0300

    updated firebase.js
    removed API KEy

--- src/firebase.js
+++ src/firebase.js
@@ -3,10 +3,10 @@ import {getAuth} from 'firebase/auth' //for user authentication
 import { getFirestore } from 'firebase/firestore' //for db op
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB9UsNFAY6lgEZa3DWfBFAtr5fFeBdmS_A", //to identify my project
-  authDomain: "task-manager-app-87a13.firebaseapp.com", // for auth.
+  apiKey: API_KEY, //to identify my project
+  authDomain: *** , // for auth.
   projectId: "task-manager-app-87a13", // for firestore db 
-  storageBucket: "task-manager-app-87a13.firebasestorage.app", 
+  storageBucket: *** , 
   messagingSenderId: "243535462632", //for push notification
   appId: "1:243535462632:web:99655676dbe5d3afa9895d"
 };
